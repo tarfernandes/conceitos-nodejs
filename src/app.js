034a2d49a7bbedbd7a8e690afcbbff68a9c02587
tarfernandes <tarcisio_fernandez@hotmail.com>
2020-06-04 01:03:00 -0300

    Projects CRU, Likes 50%

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +15,21 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  };
+
+  repositories.push(repository);
+
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,7 +41,14 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  repository.likes += 1;
+
+  return response.json(repository);
+
 });
 
 module.exports = app;
