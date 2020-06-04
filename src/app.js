a2f975d9c4e9139fd8accac2307e4009d7e1506a
tarfernandes <tarcisio_fernandez@hotmail.com>
2020-06-04 01:50:12 -0300

    Projects CRU, Likes 70%

--- src/app.js
+++ src/app.js
@@ -11,7 +11,15 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.query; 
+  
+  const results = title
+    ? repositories.filter(repositories => repositories.title.includes(title))
+    : repositories;
+
+  return response.json(results);
+
+
 });
 
 app.post("/repositories", (request, response) => {
@@ -22,7 +30,7 @@ app.post("/repositories", (request, response) => {
     title,
     url,
     techs,
-    likes:0,
+    likes:0
   };
 
   repositories.push(repository);
@@ -33,11 +41,39 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoriesIndex = repositories.findIndex(repository => repository.id == id);
+
+  if ( repositoriesIndex < 0) {
+    return response.status(400).json({error:'Repository not found.' })
+  }
+
+  const repository = {
+    title, 
+    url, 
+    techs,
+  }
+
+  repositories[projectIndex] = repository;
+
+  return  response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoriesIndex = repositories.findIndex( repository => repository.id == id);
+
+  if ( repositoriesIndex < 0 ) {
+    return response.status(400).json({error: 'Repository not found.'})
+  }
+
+  repositories.splice(repositoriesIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
